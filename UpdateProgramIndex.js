89644258a2a26ee5e4e7d0941bb7c6e241f68c4c
Reginald Gillespie <reginaldgillespie@protommail.com>
2024-05-13 15:14:52 -0500

    Push from push.ps1

--- UpdateProgramIndex.js
+++ UpdateProgramIndex.js
@@ -2,21 +2,30 @@
 
 const fs = require("fs");
 const https = require('https');
-const url = 'https://api.coindesk.com/v1/bpi/currentprice.json';
-https.get(url, (response) => {
-  let data = '';
-  response.on('data', (chunk) => {
-    data += chunk;
-  });
-  response.on('end', () => {
-    const json = JSON.parse(data);
-    const price = json.bpi.USD.rate_float.toFixed(2);
-    const fileContents = `var price = "${price}";`;
-    fs.writeFile('test.js', fileContents, (err) => {
-      if (err) throw err;
-      console.log('Bitcoin price updated successfully!');
-    });
-  });
-}).on('error', (err) => {
-  console.log('Error: ' + err.message);
-});
+const fetch = require('node-fetch-commonjs');
+
+// Async function so we can await
+(async function main(){
+    const response = await (await fetch("https://api.coindesk.com/v1/bpi/currentprice.json")).text()
+    fs.writeFileSync('test.txt', response);
+    console.log("Finished")
+})()
+
+// const url = 'https://api.coindesk.com/v1/bpi/currentprice.json';
+// https.get(url, (response) => {
+//   let data = '';
+//   response.on('data', (chunk) => {
+//     data += chunk;
+//   });
+//   response.on('end', () => {
+//     const json = JSON.parse(data);
+//     const price = json.bpi.USD.rate_float.toFixed(2);
+//     const fileContents = `var price = "${price}";`;
+//     fs.writeFile('test.js', fileContents, (err) => {
+//       if (err) throw err;
+//       console.log('Bitcoin price updated successfully!');
+//     });
+//   });
+// }).on('error', (err) => {
+//   console.log('Error: ' + err.message);
+// });
